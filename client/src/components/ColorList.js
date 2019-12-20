2b537c32007b5e7e623e2d46cc5d2e433e41b4b6
AbdelI <homeworkoracle@protonmail.com>
2019-12-20 09:50:32 +0100

    AxiosAuth function created,OnvalueChange,GET request

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
-import axios from "axios";
-
+import { AxiosAuth } from "./AxiosAuth";
 const initialColor = {
   color: "",
   code: { hex: "" }
 };
 
-const ColorList = ({ colors, updateColors }) => {
+const ColorList = ({ colors, updateColors, getAllColors }) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
@@ -16,15 +15,24 @@ const ColorList = ({ colors, updateColors }) => {
     setColorToEdit(color);
   };
 
-  const saveEdit = e => {
+  const saveEdit = (e, id) => {
     e.preventDefault();
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    AxiosAuth()
+      .put(`/colors/${id}`, colorToEdit)
+      .then(res => setColorToEdit(res.data))
+      .catch(err => console.log(err));
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    AxiosAuth()
+      .delete(`colors/${color}`)
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+    getAllColors();
   };
 
   return (
@@ -34,13 +42,15 @@ const ColorList = ({ colors, updateColors }) => {
         {colors.map(color => (
           <li key={color.color} onClick={() => editColor(color)}>
             <span>
-              <span className="delete" onClick={e => {
-                    e.stopPropagation();
-                    deleteColor(color)
-                  }
-                }>
-                  x
-              </span>{" "}
+              <span
+                className="delete"
+                onClick={e => {
+                  e.stopPropagation();
+                  deleteColor(color.id);
+                }}
+              >
+                x
+              </span>
               {color.color}
             </span>
             <div
@@ -51,7 +61,7 @@ const ColorList = ({ colors, updateColors }) => {
         ))}
       </ul>
       {editing && (
-        <form onSubmit={saveEdit}>
+        <form onSubmit={e => saveEdit(e, colorToEdit.id)}>
           <legend>edit color</legend>
           <label>
             color name:
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import { AxiosAuth } from "./AxiosAuth";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
-const Login = () => {
+const Login = props => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const [userCredentials, setUserCredentials] = useState({
+    username: "",
+    password: ""
+  });
+
+  const onFormValueChange = event => {
+    const { name, value } = event.target;
+    setUserCredentials({
+      ...userCredentials,
+      [name]: value
+    });
+  };
+
+  const onLogin = e => {
+    e.preventDefault();
+    AxiosAuth()
+      .post("/login", userCredentials)
+      .then(res => {
+        console.log(res);
+        window.localStorage.setItem("token", res.data.payload);
+        alert("login successful !");
+        props.history.push("/bubble-page");
+      })
+      .catch(err => console.log("not working", err));
+  };
   return (
-    <>
+    <Router>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+      <form onSubmit={onLogin}>
+        <input
+          type="text"
+          name="username"
+          value={userCredentials.username}
+          placeholder="enter user id"
+          onChange={onFormValueChange}
+        />
+        <input
+          type="password"
+          name="password"
+          value={userCredentials.password}
+          placeholder="enter password"
+          onChange={onFormValueChange}
+        />
+        <button>Log in</button>
+      </form>
+      <Link to="/bubble-page">Bubble Page</Link>
+    </Router>
   );
 };
 
